9a8bbee92484ab0a8c01478d6fc4bbeaa3cfa2b9
NecroReindeer <necroreindeer@gmail.com>
2016-03-13 15:03:11 +0000

    Tidied code for Arduino

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,42 +1,44 @@
 #include <Keyboard.h>
 
+// String that will be read from serial port
 String incomingString = "";
 
-
-int upLedPin = 13;
-int rightLedPin = 12;
-int downLedPin = 11;
-int leftLedPin = 10;
+const int upLedPin = 13;
+const int rightLedPin = 12;
+const int downLedPin = 11;
+const int leftLedPin = 10;
+// LEDs listed in this order to correspond with serial string from game
 int leds[] = {leftLedPin, downLedPin, upLedPin, rightLedPin};
 
 const int upButtonPin = 2;
 const int rightButtonPin = 3;
 const int downButtonPin = 4;
 const int leftButtonPin = 5;
+// Buttons listed in same order as keys so that loop can be used
 int buttons[] = {leftButtonPin, downButtonPin, upButtonPin, rightButtonPin};
+int directionKeys[] = {KEY_LEFT_ARROW, KEY_DOWN_ARROW, KEY_UP_ARROW, KEY_RIGHT_ARROW};
 
-char directionKeys[] = {KEY_LEFT_ARROW, KEY_DOWN_ARROW, KEY_UP_ARROW, KEY_RIGHT_ARROW};
-
+// Whether the LED is on or off, set from the serial string
 int ledState = 0;
+
+// Whether the button is on or off, read from the digital pins
 int buttonState = 0;
 
 void setup() 
 {
-  pinMode(upLedPin, OUTPUT);
-  pinMode(rightLedPin, OUTPUT);
-  pinMode(leftLedPin, OUTPUT);
-  pinMode(downLedPin, OUTPUT);
-  pinMode(upButtonPin, INPUT);
-  pinMode(downButtonPin, INPUT);
-  pinMode(rightButtonPin, INPUT);
-  pinMode(leftButtonPin, INPUT);
-
+  // Set LEDs and buttons to output and input respectively
+  for (int i = 0; i < 4; i++)
+  {
+    pinMode(leds[i], OUTPUT);
+    pinMode(buttons[i], INPUT);
+  }
 }
 
 void loop() 
 {  
   if (Serial.available() > 0) 
   {
+    // Using 9 to terminate the string
     incomingString = Serial.readStringUntil('9');
 
     for (int i = 0; i < 4; i++)
